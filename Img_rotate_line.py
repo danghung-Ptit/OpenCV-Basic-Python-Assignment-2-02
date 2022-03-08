f0eec78e78985bfee53b9e86cd55a3b898254f09
danghung-Ptit <61653571+danghung-Ptit@users.noreply.github.com>
2022-03-08 23:47:04 +0700

    Add files via upload

---
+++ Img_rotate_line.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import cv2
+import numpy as np
+import imutils
+
+# Đường dẫn ảnh, các bạn đổi tên file tại đây để thử nhé
+img_path = "/Users/abc/Documents/Code_doAn/YOLOv5/data_test/0B18DCDT100BS.png"
+img = cv2.imread(img_path)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+Canny = cv2.Canny(gray, 100, 200)
+try:
+	angles = []
+	lines = cv2.HoughLines(Canny, rho=1, theta=np.pi/180, threshold=100)
+	for line in lines:
+		rho, theta = line[0]
+		a = np.cos(theta)
+		b = np.sin(theta)
+		x0 = a*rho
+		y0 = b*rho
+		x1 = int(x0 + 1000*(-b))
+		y1 = int(y0 + 1000*(a))
+		x2 = int(x0 - 1000*(-b))
+		y2 = int(y0 - 1000*(a))
+		cv2.line(img,(x1,y1),(x2,y2),(255,0,0),2)
+		angles.append((y1 - y2) / (x1 - x2) * (180 / np.pi))
+	angle = sum(angles) / len(angles)
+	print("góc nghiêng {}".format(angle))
+except:
+	angle = 0
+	
+	
+def main():	
+	Ivehicle = imutils.rotate(img, angle= angle) # hàm xoay ảnh
+	cv2.imshow("Img original", img)
+	cv2.imshow("Img clean", Ivehicle)
+	cv2.waitKey()
+	cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+	main()
---
+++ img_note.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import cv2
+
+img = np.zeros((512,512,3), np.uint8) # tạo nền đen
+img1 = cv2.line(img,(150,200),(400,200),(255,0,0),5) # vẽ đừờng thẳng
+img2 = cv2.rectangle(img,(100,450),(450,300),(0,255,0),3) # vẽ hình chữ nhật
+img3 = cv2.circle(img,(262,100), 63, (100,0,255), 3) # vẽ hình tròn
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img,'Hung Dang',(100,400), font, 2,(200,50,100),2,cv2.LINE_AA)
+
+def main():	
+	cv2.imshow('IMG_line', img)
+	cv2.waitKey()
+	cv2.destroyAllWindows()
+	
+if __name__ == '__main__':
+	main()
